8c0c2ea7708ec54cb3f9e9719e39a142debb7035
PERPGamer <copsclanleaderHD@gmail.com>
2020-01-09 15:40:11 -0500

    Fix for Lost MC Garage

--- client.lua
+++ client.lua
@@ -168,7 +168,29 @@ Citizen.CreateThread(function()
 	RequestIpl("ch_int_placement_ch_interior_7_dlc_casino_utility_milo_")
 	RequestIpl("ch_int_placement_ch_interior_8_dlc_tunnel_milo_")
 	RequestIpl("ch_int_placement_ch_interior_9_dlc_casino_shaft_milo_")
+	RequestIpl("gabz_biker_milo_")
+
+	interiorID = GetInteriorAtCoords(994.47870000, -122.99490000, 73.11467000)
 	
+	
+	if IsValidInterior(interiorID) then
+	EnableInteriorProp(interiorID, "walls_02")
+	SetInteriorPropColor(interiorID, "walls_02", 8)
+	EnableInteriorProp(interiorID, "Furnishings_02")
+	SetInteriorPropColor(interiorID, "Furnishings_02", 8)
+	EnableInteriorProp(interiorID, "decorative_02")
+	EnableInteriorProp(interiorID, "mural_03")
+	EnableInteriorProp(interiorID, "lower_walls_default")
+	SetInteriorPropColor(interiorID, "lower_walls_default", 8)
+	EnableInteriorProp(interiorID, "mod_booth")
+	EnableInteriorProp(interiorID, "gun_locker")
+	EnableInteriorProp(interiorID, "cash_small")
+	EnableInteriorProp(interiorID, "id_small")
+	EnableInteriorProp(interiorID, "weed_small")
+	
+	RefreshInterior(interiorID)
+	
+	end
 	
 -- Vault
 	local interiorid = GetInteriorAtCoords(2488.348, -267.3637, -71.64563)
